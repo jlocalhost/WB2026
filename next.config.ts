34b48a4c4c38072220ee2e4e73344cc300d26627
jlocalhost <shivang.sharma@jarvis.consulting>
2026-03-12 00:40:34 +0530

    Build issue fixed

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
-  experimental: isDev ? {
+  experimental: (isDev ? {
     allowedDevOrigins: ["http://localhost:3000", "http://192.168.0.97:3000", "http://192.168.101.14:3000"],
-  } : {},
+  } : {}) as any,
 };
 
 export default nextConfig;
